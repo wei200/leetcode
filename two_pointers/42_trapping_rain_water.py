57a6db054ed5a863e578520bd002a82f6d24d7f3
weiweiutd@gmail.com <weiweiutd@gmail.com>
2020-07-19 00:40:53 -0500

    add more solutions

---
+++ two_pointers/42_trapping_rain_water.py
@@ -0,0 +1,68 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+        ## brute force
+        # Time:  O(3N) ~ O(N)
+        # Space: O(N) since two list was created with both N
+        ## for amount of trapped water for each bar i is min(left_max,right+max) - height[i]
+#         if not height:
+#             return 0
+        
+#         res = 0
+#         n = len(height)
+        
+#         left_max = [0] * n
+#         right_max = [0] * n
+        
+#         left_max[0] = height[0]
+#         # loop from left to right, find the left_max for each bar
+#         for i in range(1, n):
+            
+#             left_max[i] = max(height[i], left_max[i - 1])
+        
+#         right_max[n - 1] = height[n - 1]
+#         # loop from right to left, find the right_max for each bar
+#         for j in range(n - 2, -1, -1):
+#             right_max[j] = max(height[j], right_max[j + 1])
+            
+#         # loop over the list again to calculate the amount of trapped water
+#         # the first and last bar don't trapped water, so count from height[1] to height[-2]
+        
+#         for i in range(1, n-1):
+#             res += min(left_max[i], right_max[i]) - height[i]
+#         return res
+    
+         ## two pointer
+        # Time: O(n), since single iteration of O(n)
+        # Space: O(1), only constant space required for left, right, left_max and right_max
+        
+        if not height:
+            return 0
+        
+        left_max = 0
+        right_max = 0
+        res = 0
+        
+        left, right = 0
+        right = len(height) - 1
+        
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] < left_max:
+                    res += left_max - height[left]
+                else:
+                    left_max = height[left]
+                left += 1
+            else:
+                if height[right] < right_max:
+                    res += right_max - height[right]
+                else:
+                    right_max = height[right]
+                    right -= 1
+        return res
+    
+    
