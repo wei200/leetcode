d2f4ace9abbaf26f0c456d4f2e9b44481a3efae1
weiweiutd@gmail.com <weiweiutd@gmail.com>
2020-07-24 00:41:26 -0500

    subsets

---
+++ subsets/78_subsets.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        if nums is None:
+            return None
+        
+        subsets = []
+        # start by adding the empty subset
+        subsets.append([])
+        for currentNumber in nums:
+        # we will take all existing subsets and insert the current number in them to create new subsets
+            n = len(subsets)
+            for i in range(n):
+                # create a new subset from the existing subset and insert the current element to it
+                set = list(subsets[i])
+                set.append(currentNumber)
+                subsets.append(set)
+
+        return subsets
+        
+        
+        
+        
